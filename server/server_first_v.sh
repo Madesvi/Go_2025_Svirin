d228ca3f489e82c04e01505cd0d9ed403dcba089
Madesvi <msvbui@gmail.com>
2025-11-03 14:29:52 +0300

    update files

--- server/server_first_v.sh
+++ server/server_first_v.sh
@@ -18,23 +18,21 @@ function handleRequest() {
       			break
     		fi
 
-			HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
-			[[ "$trline" =~ $HEADLINE_REGEX ]] &&
-			REQUEST=$(echo $trline | sed -E "s/$HEADLINE_REGEX/\1 \2/")
+		HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
+
+		[[ "$trline" =~ $HEADLINE_REGEX ]] &&
+		REQUEST=$(echo $trline | sed -E "s/$HEADLINE_REGEX/\1 \2/")
 
   	done
 
 	case "$REQUEST" in                                                                   
-		"GET /") 
-			RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n</h1>PONG</h1>"
-			;;
-		*) 
-			RESPONSE="HTTP/1.1 404 NotFound\r\n\r\n\r\nNot Found"
-			;;
+		"GET /") RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n</h1>PONG</h1>" ;;
+			*) RESPONSE="HTTP/1.1 404 NotFound\r\n\r\n\r\nNot Found" ;;
  	esac
 
-	# Send response
 	echo -e $RESPONSE > response
+
+  	echo -e 'HTTP/1.1 200\r\n\r\n\r\n</h1>PONG</h1>' > response
 }
 
 echo 'Listening on 3000...'
